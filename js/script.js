c308b61bd6656705906b21acd3ce8fe41f73114d
Aaron Goold <gooldster@gmail.com>
2018-01-31 11:11:30 -0800

    added jquery popup donation section

--- js/script.js
+++ js/script.js
@@ -250,5 +250,19 @@ $(document).ready(function(){
     dom = 1;
     get_rate(1,'now');
   });
+  // jQuery donation div goes here.
+
+    //----- OPEN
+  $('[data-popup-open]').on('click', function(e)  {
+  var targeted_popup_class = jQuery(this).attr('data-popup-open');
+  $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+  e.preventDefault();
+  });
+  //----- CLOSE
+  $('[data-popup-close]').on('click', function(e)  {
+  var targeted_popup_class = jQuery(this).attr('data-popup-close');
+  $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+  e.preventDefault();
+  });
 
 });//ready
